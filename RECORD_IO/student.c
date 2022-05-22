e327ebd3e784251ed4a6ccd13b93e26bac4f2f8a
SeongHunTed <4047ksh@naver.com>
2022-05-23 02:34:55 +0900

    RECORD_IO : append_done, Searching strcmp  Error

--- RECORD_IO/student.c
+++ RECORD_IO/student.c
@@ -65,36 +65,39 @@ void main(int argc, char *argv[])
 
 			if(argc < 3){
 				fprintf(stderr, "Usage : %s -s <Filename> <field_name=filed_value>", argv[0]);
-				exit(1);
+				break;
 			}
 			
 			// keyvalue 분류
 			char keyvalue[30] = {0};
-			for(int i = 0; i<strlen(argv[3]); i++){
+			int i = 0;
+			for(i = 0; i<strlen(argv[3]); i++){
 				if(argv[3][i] == '='){
 					i++;
 					int a = i;
 					for(int j = 0; j<strlen(argv[3])-a; j++){
 						keyvalue[j] = argv[3][i++];
 					}
+					keyvalue[++i] = '\0';
 					break;
 				}
 			}
-
+			printf("Keyvalue : %s \n", keyvalue);
 
 			// field_name 분류
 			char enumbuf[35] = {0};
 			sscanf(argv[3], "%s", enumbuf);
 			enum FIELD f = getFieldID(enumbuf);
+			printf("FIELD : %d\n", f);
 
 			searchRecord(fp, f, keyvalue);
-
+			fclose(fp);
 			break;
 		
 		case 'a':	// append option
 			if(argc < 8){
 				fprintf(stderr, "Usage : %s -a <Filename> <Student ID> <Student name> <Student dept> <Student Addr> <Student Email> \n", argv[0]);
-				exit(1);
+				break;
 			}
 			// bound 예외처리
 			// open 예외처리
@@ -113,13 +116,11 @@ void main(int argc, char *argv[])
 			fprintf(stderr, "Wrong Option!\n");
 			break;
 	}
-	// exit(0);
+	fclose(fp);
 }
 
 int writeRecord(FILE *fp, const STUDENT *s, int rrn)
 {
-	// recordbuf size 조정??
-	printf("I'm Writing.....\n");
 	char recordbuf[RECORD_SIZE] = {0};
 	pack(recordbuf, s);
 
@@ -131,7 +132,6 @@ int writeRecord(FILE *fp, const STUDENT *s, int rrn)
 }
 void pack(char *recordbuf, const STUDENT *s)
 {
-	printf("Packing for Write\n");
 	int i = 0;
 	int j = 0;
 	for(i = 0; i < strlen(s->id); i++){
@@ -149,6 +149,7 @@ void pack(char *recordbuf, const STUDENT *s)
 	for(i = 0; i < strlen(s->email); i++){
 		recordbuf[j++] = s->email[i];
 	} recordbuf[j++] = '#';
+	recordbuf[j] = '\0';
 }
 
 int appendRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *email)
@@ -195,36 +196,67 @@ int appendRecord(FILE *fp, char *id, char *name, char *dept, char *addr, char *e
 }
 
 int readRecord(FILE *fp, STUDENT *s, int rrn)
-{
-	char recordbuf[80];
+{	
+	rewind(fp);
+	char recordbuf[RECORD_SIZE] ={0};
 
-	memset(recordbuf, 0, 80);
-	fseek(fp, HEADER_SIZE + RECORD_SIZE * rrn, SEEK_SET);
-	fread(recordbuf, 80, 1, fp);
-	printf("%s", recordbuf);
-	unpack(recordbuf, s);
-	// if(fread(recordbuf, 80, 1, fp) > 0){
-	// 	printf("fuck");
-	// 	for(int i = 0; i < 80; i++){
-	// 		printf("%c ", recordbuf[i]);
-	// 	}
-	// 	unpack(recordbuf, s);
-	// 	return 1;
-	// } else return 0;
+	printf("%d \n", HEADER_SIZE + RECORD_SIZE * rrn);
+	fseek(fp, HEADER_SIZE + (RECORD_SIZE * rrn), SEEK_SET);
+	if(fread(recordbuf, RECORD_SIZE, 1, fp) > 0){
+		unpack(recordbuf, s);
+		return 1;
+	} else return 0;
 
 }
 
 
 void unpack(const char *recordbuf, STUDENT *s)
 {
-	int i;
-	for(i = 0; i<80; i++){
-		if(recordbuf[i] == '#'){
-			continue;
-		}
-		s->id[i] = recordbuf[i];
-	}
+	int j = 0;
+	int i = 0;
 	
+	for(i = 0; i<9; i++){
+		if(recordbuf[j] == '#') break;
+		s->id[i] = recordbuf[j];
+		j++;
+	}
+	s->id[j] = '\0';
+	j++;
+
+
+	for(i = 0; i<11; i++){
+		if(recordbuf[j] == '#') break;
+		s->name[i] = recordbuf[j];
+		j++;
+	}
+	s->name[j] = '\0';
+	j++;
+
+
+	for(i = 0; i < 13; i++){
+		if(recordbuf[j] == '#') break;
+		s->dept[i] = recordbuf[j];
+		j++;
+	}
+	s->dept[j] = '\0';
+	j++;
+
+
+	for(i = 0; i < 31; i++){
+		if(recordbuf[j] == '#') break;
+		s->addr[i] = recordbuf[j];
+		j++;
+	}
+	s->addr[j] = '\0';
+	j++;
+
+	for(i = 0; i < 21; i++){
+		if(recordbuf[j] == '#') break;
+		s->email[i] = recordbuf[j];
+		j++;
+	}
+	s->email[++i] = '\0';
+
 }
 
 //
@@ -240,15 +272,82 @@ void searchRecord(FILE *fp, enum FIELD f, char *keyval)
 	fseek(fp, (off_t)0 , SEEK_END);
 	int rrn = ftell(fp) / RECORD_SIZE;
 	STUDENT s;
+	// memset(&s.id, 0 , sizeof(s.id));
+	memset(&s.name, 0, sizeof(s.name));
+	memset(&s.dept, 0, sizeof(s.dept));
+	memset(&s.addr, 0, sizeof(s.addr));
+	memset(&s.email, 0, sizeof(s.email));
 
-	char idbuf[8] = {0};
+
+	char idbuf[8];
+	char namebuf[10];
+	char deptbuf[12];
+	char addrbuf[30];
+	char emailbuf[20];
 	
-	if(f == ID){
+	memset(idbuf, 0, sizeof(idbuf));
+	memset(namebuf, 0, sizeof(namebuf));
+	memset(deptbuf, 0, sizeof(deptbuf));
+	memset(addrbuf, 0, sizeof(addrbuf));
+	memset(emailbuf, 0, sizeof(emailbuf));
+
+	
+ 	if(f == ID){
 		for(int i = 0; i<rrn; i++){
-			fseek(fp, HEADER_SIZE + RECORD_SIZE * rrn, SEEK_SET);
+			fseek(fp, HEADER_SIZE + RECORD_SIZE * i, SEEK_SET);
 			fread(idbuf, 8, 1, fp);
-			if(strcmp(keyval, idbuf)){
-				readRecord(fp, &s, rrn);
+			printf("IDbuf : %s \n", idbuf);
+			printf("Keyvalue : %s\n", keyval);
+			printf("strcmp : %d\n", strcmp(keyval, idbuf));
+			if(strcmp(keyval, idbuf) == -3){
+				printf("In\n");
+				readRecord(fp, &s, i);
+				printRecord(&s);
+			}	
+		}
+	}
+
+	if(f == NAME){
+		for(int i = 0; i<rrn; i++){
+			fseek(fp, 8 + HEADER_SIZE + RECORD_SIZE * i, SEEK_SET);
+			fread(namebuf, 10, 1, fp);
+			printf("namebuf : %s\n", namebuf);
+			if(strcmp(keyval, namebuf) == -3){
+				readRecord(fp, &s, i);
+				printf("strcmp : %d\n", strcmp(keyval, namebuf));
+				printRecord(&s);
+			}	
+		}
+	}
+
+	if(f == DEPT){
+		for(int i = 0; i<rrn; i++){
+			fseek(fp, 18 + HEADER_SIZE + RECORD_SIZE * i, SEEK_SET);
+			fread(deptbuf, 8, 1, fp);
+			if(strcmp(keyval, deptbuf) == 0){
+				readRecord(fp, &s, i);
+				printRecord(&s);
+			}	
+		}
+	}
+
+	if(f == ADDR){
+		for(int i = 0; i<rrn; i++){
+			fseek(fp, 30 + HEADER_SIZE + RECORD_SIZE * i, SEEK_SET);
+			fread(addrbuf, 8, 1, fp);
+			if(strcmp(keyval, addrbuf) == 0){
+				readRecord(fp, &s, i);
+				printRecord(&s);
+			}	
+		}
+	}
+
+	if(f == EMAIL){
+		for(int i = 0; i<rrn; i++){
+			fseek(fp, 60 + HEADER_SIZE + RECORD_SIZE * i, SEEK_SET);
+			fread(emailbuf, 8, 1, fp);
+			if(strcmp(keyval, emailbuf) == 0){
+				readRecord(fp, &s, i);
 				printRecord(&s);
 			}	
 		}
@@ -267,16 +366,19 @@ enum FIELD getFieldID(char *fieldname)
 {	
 	char enumcheck[6] = {0};
 
-	for(int i = 0; i < strlen(fieldname); i++){
+	int i = 0;
+	for(i = 0; i < strlen(fieldname); i++){
 		if(fieldname[i] == '='){
 			break;
 		}
 		enumcheck[i] = fieldname[i];
 	}
+	enumcheck[i] = '\0';
 
 	if(strcmp(enumcheck, "ID") == 0) return ID;
-	if(strcmp(enumcheck, "NAME") == 0) return NAME;
-	if(strcmp(enumcheck, "DEPT") == 0) return DEPT;
-	if(strcmp(enumcheck, "ADDR") == 0) return ADDR;
-	if(strcmp(enumcheck, "EMAIL") == 0) return EMAIL;
+	else if(strcmp(enumcheck, "NAME") == 0) return NAME;
+	else if(strcmp(enumcheck, "DEPT") == 0) return DEPT;
+	else if(strcmp(enumcheck, "ADDR") == 0) return ADDR;
+	else if(strcmp(enumcheck, "EMAIL") == 0) return EMAIL;
+	else return 0;
 }
